93520016ba8d35499bd2e493c4915b3dd31da25c
mszeles <miklos.szeles@gmail.com>
2020-10-28 19:14:20 +0100

    Implementing Spring conf for Services. Closes #22.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -16,12 +16,14 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
-    }
 
-    @Override
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+		super();
+		this.ownerService = ownerService;
+		this.vetService = vetService;
+	}
+
+	@Override
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
