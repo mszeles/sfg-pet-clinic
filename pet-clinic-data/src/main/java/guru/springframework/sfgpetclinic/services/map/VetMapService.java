51f2805a87d346c2197ddbe7d4403108d322d24b
mszeles <miklos.szeles@gmail.com>
2020-11-06 21:13:19 +0100

    Create Spring Data JPA Vet Specialty Service. Closes #44 Renamed some map service to be consistent

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -10,11 +10,11 @@ import guru.springframework.sfgpetclinic.services.SpecialityService;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Service
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
 	private final SpecialityService specialityService;
 	
-	public VetServiceMap(SpecialityService specialityService) {
+	public VetMapService(SpecialityService specialityService) {
 		super();
 		this.specialityService = specialityService;
 	}
